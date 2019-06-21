e0f59c356c613a438cddee40ad1d5fe97ad63b93
z20818z <49013725+z20818z@users.noreply.github.com>
2019-06-21 17:56:15 +0800

    Add files via upload

--- work2/js/calender.js
+++ work2/js/calender.js
@@ -62,6 +62,7 @@ function last(){
     calender(thisYear,thisMonth);
 }
 function today(){
+    thisMonth = dt.getMonth();
     changeWeek(thisDate,thisDay);
     $(".ta").remove();
     calender(dt.getFullYear(),dt.getMonth());
@@ -96,9 +97,14 @@ function changeWeek(date,days){
     for(i=1;i<8;i++){
         if(thisMonth == dt.getMonth()){
             if($("#day"+i).html()==thisDate){
+                console.log("換色");
                 $("#day" + i).css("color","blue");
+            }else{
+                console.log("不換色");
+                $("#day" + i).css("color","black");
             }
         }else{
+            console.log("不換色");
             $("#day" + i).css("color","black");
         }
     }
@@ -115,6 +121,10 @@ function setdays(x,y,days){
             $("#day"+i).html(x);
             
             ajax_update(thisYear,thisMonth+1,x);
+            for(time=0;time<24;time++){
+                ajax_update_time(thisYear,thisMonth+1,x,time);
+            }
+            
             x-=1;
         }else{
             x-=1;
@@ -125,6 +135,9 @@ function setdays(x,y,days){
         if(y<=monthDays[thisMonth]){
             $("#day"+i).html(y);
             ajax_update(thisYear,thisMonth+1,y);
+            for(time=0;time<24;time++){
+                ajax_update_time(thisYear,thisMonth+1,y,time);
+            }
             y+=1;
         }else{
             y+=1;
@@ -146,8 +159,15 @@ function createTable(){
         append_table += '<td onclick=recordAct() id="day'+ i + 'sch' + '"'+'</td>';
     }
     append_table += "'</tr>'";
+    for(i=0;i<24;i++){
+        append_table += '<tr><td>'+i+':00'+'</td>' + '<td onclick=recordAct() id="day'+ 1 + 'time' + i + '"'+'</td>'+'<td onclick=recordAct() id="day'+ 2 + 'time' + i + '"'+'</td>'+
+        '<td onclick=recordAct() id="day'+ 3 + 'time' + i + '"'+'</td>' + '<td onclick=recordAct() id="day'+ 4 + 'time' + i + '"'+'</td>' + '<td onclick=recordAct() id="day'+ 5 + 'time' + i + '"'+'</td>' + 
+        '<td onclick=recordAct() id="day'+ 6 + 'time' + i + '"'+'</td>' + '<td onclick=recordAct() id="day'+ 7 + 'time' + i + '"'+'</td>' +  
+        '</tr>';
+    }
     $('#schedule').append(append_table);
 }
+
 function ajax_update(year,month,date){
     month = add_zero_m(month);
     date = add_zero_d(date);
@@ -164,9 +184,27 @@ function ajax_update(year,month,date){
     
     
 }
+function ajax_update_time(year,month,date,time){
+    month = add_zero_m(month);
+    date = add_zero_d(date);
+    date = year+'-'+month+'-'+date;
+    $.ajax({
+        type:'GET',
+        url: "receive_time.php", 
+        data:{'date': date,'time':time,'user':user},
+        dataType:'html',
+        success: function(data){
+            console.log("TIME" + data);
+            console.log(date+'-'+time+":00");
+            $("."+date+'-'+time).html(data);
+    }});
+    
+    
+}
 function add_class(){
     month = add_zero_m(thisMonth+1);
     dates = add_zero_d($("#day1").html());
+    if($("#day1").html()){
         for(day=1;day<8;day++){
             x = day-1;
             x = parseInt(x) + parseInt(dates);
@@ -175,9 +213,23 @@ function add_class(){
             $("#day"+day + 'sch').addClass(thisYear + '-' + month + '-' + x + 'sch');
             for(i = 0; i<= 23; i++){
                 $("#day"+day + 'time' + i).removeClass();
-                $("#day"+day + 'time' + i).addClass(thisYear + '-' + month + '-' + x + '-' + i + ':00');
+                $("#day"+day + 'time' + i).addClass(thisYear + '-' + month + '-' + x + '-' + i);
             }
         }
+    }else{
+        dates = add_zero_d($("#day7").html());
+        for(day=7;day>0;day--){
+            x = day-7;
+            x = parseInt(x) + parseInt(dates);
+            console.log("日期:" + x);
+            $("#day"+day).addClass(thisYear + '-' + month + '-' + x);
+            $("#day"+day + 'sch').addClass(thisYear + '-' + month + '-' + x + 'sch');
+            for(i = 0; i<= 23; i++){
+                $("#day"+day + 'time' + i).removeClass();
+                $("#day"+day + 'time' + i).addClass(thisYear + '-' + month + '-' + x + '-' + i);
+            }
+        }
+    }
     
     
     
